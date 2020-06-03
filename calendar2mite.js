85b2e3302cf5657dba85f340609a4ef8a9042927
moritz.wachter <moritz.wachter@exozet.com>
2020-06-03 22:10:56 +0200

    reimplementing the workflow with Promise.all

--- calendar2mite.js
+++ calendar2mite.js
@@ -1,13 +1,26 @@
 const googleAuth = require('./GoogleAuth.js');
 const GoogleCalendar = require('./GoogleCalendar');
 const dateTime = require('./DateTimeHelper');
+const eventMapper = require('./EventMapper');
+const mite = require('./Mite');
 
 googleAuth.auth(runApplication);
 
 function runApplication(auth) {
     const calendar = new GoogleCalendar(auth);
-    calendar.getEvents(dateTime.getTimeMin(2), dateTime.getTimeMax())
-        .then(events => console.log(events))
+
+    const getEvents = calendar.getEvents(dateTime.getTimeMin(2), dateTime.getTimeMax());
+    const getMappings = eventMapper.getMappings();
+
+    Promise.all([getEvents, getMappings])
+        .then(values => {
+            const [events, mappings] = values
+
+            events.forEach(event => {
+                let entry = mite.generateEntryFormat(event, mappings);
+                mite.addEntry(entry);
+            });
+        })
         .catch(err => console.log(err))
     ;
 }
