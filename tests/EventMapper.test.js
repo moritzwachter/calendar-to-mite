63b91d1c94676244b4bebbd93c807e94b53147b4
moritz.wachter <moritz.wachter@exozet.com>
2020-06-05 09:54:18 +0200

    fixed test

---
+++ tests/EventMapper.test.js
@@ -0,0 +1,13 @@
+const EventMapper = require('../src/EventMapper.js');
+
+test('should get correct project and service mapping', () => {
+    const mockMappings = [
+        {keyword: '#321', projectId: 0, serviceId: 0},
+        {keyword: '#123', projectId: 1234, serviceId: 4321}
+    ];
+
+    const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
+
+    expect(projectId).toBe(1234);
+    expect(serviceId).toBe(4321);
+})
