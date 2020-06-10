4db0b00f1af156c01c75f70f624043b26e21d6d4
moritz.wachter <moritz.wachter@exozet.com>
2020-06-10 20:51:19 +0200

    implemented a test for generateEntryFormat

--- tests/Mite.test.js
+++ tests/Mite.test.js
@@ -11,6 +11,26 @@ jest.mock('../src/EventMapper', () => ({
     getProjectAndServiceMapping: () => [123, 456]
 }));
 
-test('construction creates new miteApi object', () => {
+test('generateEntryFormat return values', () => {
     const mite = new Mite();
+    let start = new Date('2020-01-01T00:00:00-02:00');
+    let end = new Date(start.getTime() + (60 * 60000));
+
+    const event = {
+        start: {
+            dateTime: start
+        },
+        end: {
+            dateTime: end
+        },
+        summary: 'summary'
+    };
+
+    let entry = mite.generateEntryFormat(event, []);
+
+    expect(entry.project_id).toBe(123);
+    expect(entry.service_id).toBe(456);
+    expect(entry.note).toBe('summary');
+    expect(entry.minutes).toBe(60);
+    expect(entry.date_at).toBe('2020-01-01');
 })
