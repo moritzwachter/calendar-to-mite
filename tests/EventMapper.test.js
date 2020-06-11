99305f7135fd4ca6ff6cbafa99fc5bb17247143d
moritz.wachter <moritz.wachter@exozet.com>
2020-06-11 09:05:58 +0200

    grouped tests of one method

--- tests/EventMapper.test.js
+++ tests/EventMapper.test.js
@@ -1,23 +1,25 @@
 const EventMapper = require('../src/EventMapper.js');
 
-test('should get correct project and service mapping', () => {
-    const mockMappings = [
-        {keyword: '#321', projectId: 0, serviceId: 0},
-        {keyword: '#123', projectId: 1234, serviceId: 4321}
-    ];
+describe('getProjectAndServiceMapping', () => {
+    test('should get correct project and service mapping', () => {
+        const mockMappings = [
+            {keyword: '#321', projectId: 0, serviceId: 0},
+            {keyword: '#123', projectId: 1234, serviceId: 4321}
+        ];
 
-    const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
+        const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
 
-    expect(projectId).toBe(1234);
-    expect(serviceId).toBe(4321);
-});
+        expect(projectId).toBe(1234);
+        expect(serviceId).toBe(4321);
+    });
 
-test('should return array with two null values', () => {
-    const mockMappings = [];
-    const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
+    test('should return array with two null values', () => {
+        const mockMappings = [];
+        const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
 
-    expect(projectId).toBe(null);
-    expect(serviceId).toBe(null);
+        expect(projectId).toBe(null);
+        expect(serviceId).toBe(null);
+    })
 })
 
 // @TODO: Filesystem tests / mocks
