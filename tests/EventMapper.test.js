94c2717578fce38971a8ae2ac516120498b9c8bb
moritz.wachter <moritz.wachter@exozet.com>
2020-06-14 22:35:14 +0200

    added false mapping at the end

--- tests/EventMapper.test.js
+++ tests/EventMapper.test.js
@@ -4,7 +4,8 @@ describe('getProjectAndServiceMapping', () => {
     test('should get correct project and service mapping', () => {
         const mockMappings = [
             {keyword: '#321', projectId: 0, serviceId: 0},
-            {keyword: '#123', projectId: 1234, serviceId: 4321}
+            {keyword: '#123', projectId: 1234, serviceId: 4321},
+            {keyword: '#456', projectId: 9876, serviceId: 5432}
         ];
 
         const [projectId, serviceId] = EventMapper.getProjectAndServiceMapping(mockMappings, 'summary #123');
@@ -20,6 +21,4 @@ describe('getProjectAndServiceMapping', () => {
         expect(projectId).toBe(null);
         expect(serviceId).toBe(null);
     })
-})
-
-// @TODO: Filesystem tests / mocks
+});
