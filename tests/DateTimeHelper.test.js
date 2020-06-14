39a2bdcd32d3f5048e0d8bf6341a1a6438c2fb5f
moritz.wachter <moritz.wachter@exozet.com>
2020-06-14 22:38:30 +0200

    added describe blocks

--- tests/DateTimeHelper.test.js
+++ tests/DateTimeHelper.test.js
@@ -1,16 +1,20 @@
 const DateTimeHelper = require('../src/DateTimeHelper.js');
 
-test('should return date as YYYY-MM-DD', () => {
-    let input = '2020-01-01T00:00:00-02:00';
-    let result = DateTimeHelper.formatDate(input);
+describe('formatDate', () => {
+    test('should return date as YYYY-MM-DD', () => {
+        let input = '2020-01-01T00:00:00-02:00';
+        let result = DateTimeHelper.formatDate(input);
 
-    expect(result).toBe('2020-01-01');
+        expect(result).toBe('2020-01-01');
+    });
 });
 
-test('should return 60 minutes', () => {
-    let start = new Date();
-    let end = new Date(start.getTime() + (60 * 60000));
+describe('getDurationInMinutes', () => {
+    test('should return 60 minutes', () => {
+        let start = new Date();
+        let end = new Date(start.getTime() + (60 * 60000));
 
-    let result = DateTimeHelper.getDurationInMinutes(start, end);
-    expect(result).toBe(60);
-})
+        let result = DateTimeHelper.getDurationInMinutes(start, end);
+        expect(result).toBe(60);
+    });
+});
